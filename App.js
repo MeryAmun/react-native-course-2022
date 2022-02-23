a4ac20057507d18e4754bf8cc0bc40a8a814f67e
MeryAmun <chebesiawah@gmail.com>
2022-02-23 03:48:48 +0100

    start screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from './components/Header';
+import { StartGameScreen } from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title='Number Game App'/>
+      <Header title='Guess a Number App'/>
+      <StartGameScreen/>
     </View>
   );
 }
